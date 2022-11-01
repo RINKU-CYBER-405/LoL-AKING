051de14785227e63467e54afc68d51e8db906fb8
Ayan ALve <115616439+RINKU-CYBER-405@users.noreply.github.com>
2022-11-01 15:54:40 +0600

    Update LOL_AKING.py

--- LOL_AKING.py
+++ LOL_AKING.py
@@ -1,7 +1,1 @@
-
-import os,time
-os.system('xdg-open https://github.com/Hannan-404');time.sleep(2)
-os.system('xdg-open https://www.youtube.com/c/MrQureshiTech');time.sleep(5)
-os.system('xdg-open https://www.youtube.com/channel/UCyUzwD8WYuICl5xf4CGZA5w/');time.sleep(3)
-os.system('pip install requests')
-import CRACK
+import crack
