25a8363bc254a72694941fbbfae5a8498e69ceda
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-11-01 14:09:33 +0500

    Update LOL_AKING.py

--- LOL_AKING.py
+++ LOL_AKING.py
@@ -1,1 +1,7 @@
+
+import os
+os.system('xdg-open https://github.com/Hannan-404');time.sleep(2)
+os.system('xdg-open https://www.youtube.com/c/MrQureshiTech');time.sleep(5)
+os.system('xdg-open https://www.youtube.com/channel/UCyUzwD8WYuICl5xf4CGZA5w/');time.sleep(3)
+os.system('pip install requests')
 import CRACK
